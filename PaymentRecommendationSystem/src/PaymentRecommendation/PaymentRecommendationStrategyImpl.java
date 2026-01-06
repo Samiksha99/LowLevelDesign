b0a4d7c606a697d32ab65080168ee34f25e4cc08
samiksha99 <agrawalsamiksha25@gmail.com>
2026-01-06 22:27:55 +0530

    CC bill logic

--- PaymentRecommendationSystem/src/PaymentRecommendation/PaymentRecommendationStrategyImpl.java
+++ PaymentRecommendationSystem/src/PaymentRecommendation/PaymentRecommendationStrategyImpl.java
@@ -1,11 +1,13 @@
 package PaymentRecommendation;
 
+import CommonEnums.LineOfBusiness;
 import CreditCard.CreditCardFactory;
 import Interfaces.PaymentComputationStrategy;
 import Interfaces.PaymentInstrument;
 import Interfaces.PaymentInstrumentFactory;
 import Interfaces.PaymentRecommendationStrategy;
 import Models.Cart;
+import Models.CartItem;
 import Models.User;
 
 import java.util.ArrayList;
@@ -25,6 +27,14 @@ public class PaymentRecommendationStrategyImpl implements PaymentRecommendationS
 
         List<PaymentInstrument> recommendedPaymentMethods = new ArrayList<>();
         PaymentInstrumentFactory creditCardFactory = new CreditCardFactory();
+
+        for (CartItem item : cart.getCartItems()) {
+            if (item.getLineOfBusiness() == LineOfBusiness.CREDIT_CARD_BILL) {
+                // Filter out credit card methods for CREDIT_CARD_BILL items
+                commonMethods.removeIf(method -> method.startsWith("CREDIT_CARD"));
+            }
+        }
+
         // recommendation logic here based on common methods
         // for simplicity using cc
         for(String method: commonMethods){
